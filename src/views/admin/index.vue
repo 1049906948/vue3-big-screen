8b5d5a9a023330a5f36291353b20f23772b58bfa
caokun <you@example.com>
2023-04-17 09:38:15 +0800

    admin

---
+++ src/views/admin/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="ck-container">
+
+    <div v-if="loading" class="mask flex-c">
+      <dv-loading>
+        <span class="loading-title">加载中...</span>
+      </dv-loading>
+    </div>
+
+  </div>
+</template>
+
+<script setup lang="ts">
+import { onMounted, onUnmounted, ref } from 'vue';
+const loading = ref(true);
+onMounted(() => {
+  setTimeout(() => {
+    loading.value = false;
+  }, 2000);
+})
+</script>
+
+<style scoped>
+
+</style>
