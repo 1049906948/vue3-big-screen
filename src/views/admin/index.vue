12093191b9358dfebd1c87b5e7d9eb02d6a05e96
caokun <you@example.com>
2023-04-17 10:09:19 +0800

    admin

--- src/views/admin/index.vue
+++ src/views/admin/index.vue
@@ -1,16 +1,24 @@
 <template>
-  <div class="ck-container">
+  <div class="screen-container">
+    <div class="screen-content" ref="screenRef">
 
-    <div v-if="loading" class="mask flex-c">
-      <dv-loading>
-        <span class="loading-title">加载中...</span>
-      </dv-loading>
+      <div v-if="loading" class="mask flex-c">
+        <dv-loading>
+          <span class="loading-title">加载中...</span>
+        </dv-loading>
+      </div>
+
+      <div class="ck-container" v-else>
+           1231
+      </div>
+
     </div>
-
   </div>
 </template>
 
 <script setup lang="ts">
+import windowResize from "@/utils/resize";
+const { screenRef, calcRate, windowDraw, unWindowDraw } = windowResize()
 import { onMounted, onUnmounted, ref } from 'vue';
 const loading = ref(true);
 onMounted(() => {
@@ -20,6 +28,25 @@ onMounted(() => {
 })
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+@import '@/assets/scss/common/variables.scss';
+
+  .mask {
+    position: fixed;
+    top: 0;
+    left: 0;
+    bottom: 0;
+    right: 0;
+    background-color: $theme-color;
+    z-index: 9999;
+    background-image: url('@/assets/home_bg.png');
+    .loading-title {
+      font-size: $base-font-size;
+      color: #fff;
+      margin-top: 10px;
+    }
+  }
+
+
 
 </style>
