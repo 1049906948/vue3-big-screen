29e319bd9af01d338c1adcea541584897bff9632
caokun <you@example.com>
2023-04-24 15:02:05 +0800

    web3

--- src/router/router.ts
+++ src/router/router.ts
@@ -24,7 +24,7 @@ const routes: Array<RouteRecordRaw> = [
   {
     path: '/web3',
     name: 'web3',
-    component: () => import('@/views/tailwind/test.vue')
+    component: () => import('@/views/web/test.vue')
   }
 ]
 
---
+++ src/views/web/components/Welcome.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="flex w-full justify-center items-center">
+    <div class="flex mf:flex-row flex-col items-start justify-between md:p-20 py-12 px-4">
+      <div class="flex flex-1 justify-start items-start flex-col mf:mr-10">
+        <h1 class="text-3xl sm:text-5xl text-white text-gradient py-1">
+          Send Crypto <br/> across the world
+        </h1>
+        <p class="text-left mt-5 text-white font-light md:w-9/12 w-11/12 text-base">
+          Explore the crypto world. Buy and sell cryptocurrencies easily on Krypto.
+        </p>
+        <button class="flex flex-row justify-center items-center my-5 bg-[#2952e3] p-3 rounded-full cursor-pointer hover:bg-[#2546bd]" type="button" >
+          <!--             <AiFillPlayCircle className="text-white mr-2" />-->
+          <p class="text-white text-base font-semibold">
+            Connect Wallet 连接钱包
+          </p>
+        </button>
+
+        <div class="grid sm:grid-cols-3 grid-cols-2 w-full mt-10">
+<!--          <div class="rounded-tl-2xl ${companyCommonStyles}">-->
+<!--            Reliability-->
+<!--          </div>-->
+          Reliability
+        </div>
+
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const companyCommonStyles = "min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-sm font-light text-white";
+
+</script>
+
+<style scoped>
+
+</style>
