dd959fc36b00013d45097a07d7990c8ce308ce5d
caokun <1049906948@qq.com>
2023-12-06 23:42:05 +0800

    11

--- demo/test1/1.ts
+++ demo/test1/1.ts
@@ -1,5 +1,10 @@
 type Method = "get" | "post";
 
+/**
+ * 发起网络请求
+ * @param url 请求的URL
+ * @param method 请求的方法（"get"或"post”）
+ */
 function request(url: string, method: Method) {
   if (method === "get") {
     method;
@@ -9,3 +14,4 @@ function request(url: string, method: Method) {
     const n: never = method;
   }
 }
+
