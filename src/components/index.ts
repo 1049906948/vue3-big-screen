06d81d55513aabd3675ef6e484c7bcbd857d3baf
caokun <1049906948@qq.com>
2024-01-26 18:19:07 +0800

    上传

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,9 +1,10 @@
 import mEcharts from "./echarts-custom/EchartsCustom.vue";
 import ckLoading from "./ck-loading/ckLoding.vue";
-import RightToolbar from "./RightToolbar/index.vue";
+// import RightToolbar from "./RightToolbar/index.vue";
+// import Pagination from "./Pagination/index.vue";
 // import domeComponent from "./demo-component/index";
 // 组件列表
-const components = [mEcharts, ckLoading,RightToolbar];
+const components = [mEcharts, ckLoading];
 
 // 定义 install 方法，接收 Vue 作为参数。如果使用 use 注册插件，那么所有的组件都会被注册
 const install = (Vue: any) => {
