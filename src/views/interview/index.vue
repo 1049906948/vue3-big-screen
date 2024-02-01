0dbb03d52074b4387a6be4f30c3f0e86292f4794
caokun <1049906948@qq.com>
2024-02-01 11:36:01 +0800

    居中

---
+++ src/views/interview/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="border-10 w-screen h-screen bg-amber-800">
+      <div>
+          <one-css></one-css>
+      </div>
+  </div>
+</template>
+
+<script lang="ts" setup name="interview">
+import OneCss from "@/views/interview/css/one-css.vue";
+</script>
