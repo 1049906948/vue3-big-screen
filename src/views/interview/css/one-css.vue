79c0086f098cf017ad55bba6c3ee4cf250640d96
caokun <1049906948@qq.com>
2024-02-01 12:00:57 +0800

    居中

--- src/views/interview/css/one-css.vue
+++ src/views/interview/css/one-css.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="border-10 w-screen h-screen bg-amber-800 flex items-center h-full justify-center">
 
-     <div class="w-20 h-20 bg-amber-600 flex items-center h-full justify-center text-white">
+     <div class="w-20 h-20 bg-amber-600 text-center text-white flex justify-center items-center">
        文本居中
      </div>
 
