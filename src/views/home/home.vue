5c4f215a2fed8ec25e80261f9b098c970cbf4355
1049906948 <1049906948@qq.com>
2024-08-23 06:08:57 +0000

    game

--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -103,6 +103,11 @@
             <div class="item" @click="handleUrl('/form')">表单</div>
           </el-card>
         </el-col>
+        <el-col :span="6">
+          <el-card class="item" shadow="always">
+            <div class="item" @click="handleUrl('/game')">表单</div>
+          </el-card>
+        </el-col>
       </el-row>
 
     </div>
