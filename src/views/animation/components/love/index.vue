91726bff346f67996b720f0a3b22282f275bfe74
1049906948 <1049906948@qq.com>
2024-08-26 08:03:55 +0000

    love

---
+++ src/views/animation/components/love/index.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="container">
+        <canvas ref="canvas" id="canvas" width="1400" height="600"></canvas>
+    </div>
+</template>
+
+<script setup>
+import { useDraw } from './com.config'
+import { ref, onMounted } from 'vue'
+onMounted(() => {
+    useDraw()
+})
+
+</script>
--- src/views/animation/index.vue
+++ src/views/animation/index.vue
@@ -2,11 +2,12 @@
     <ckLoading>
        <div class="bg-gray-50 min-h-screen">
           <!-- <card></card> -->
-          
+          <love></love>
        </div>
     </ckLoading>
   </template>
   
   <script lang="ts" setup>
    import Card from './components/card/card.vue'
+   import love from './components/love/index.vue'
   </script>
